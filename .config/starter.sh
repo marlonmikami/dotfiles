156277ad15dc3ef09b59146b835b64bf39dde33a
Marlon Mikami <marlonmikami@gmail.com>
2025-05-21 17:37:34 -0300

    adjustments

--- .config/starter.sh
+++ .config/starter.sh
@@ -1,4 +1,4 @@
-sudo pacman -S stow git github-cli ranger nnn btop vim w3m picom feh alacritty fastfetch alsa rofi pulseaudio polybar ttf-roboto-mono ttf-roboto-mono-nerd gucharmap acpi bspwm sxhkd xorg
+sudo pacman -S bluetui bluez bluez-utils bluez-deprecated-tools stow git github-cli ranger nnn btop vim w3m picom feh alacritty fastfetch rofi alsa pulseaudio polybar ttf-roboto-mono ttf-roboto-mono-nerd gucharmap acpi bspwm sxhkd xorg
 
 # yay -S blight
 
@@ -10,3 +10,17 @@ sudo pacman -S stow git github-cli ranger nnn btop vim w3m picom feh alacritty f
 
 # xev to show the keys being pressed
 # to fix trackpad: /etc/X11/xorg.conf.d/40-libinput.conf 
+
+
+# gh auth login
+
+
+# If ~/.inputrc doesn't exist yet: First include the original /etc/inputrc
+# so it won't get overriden
+# if [ ! -a ~/.inputrc ]; then echo '$include /etc/inputrc' > ~/.inputrc; fi
+
+# Add shell-option to ~/.inputrc to enable case-insensitive tab completion
+# echo 'set completion-ignore-case On' >> ~/.inputrc
+
+
+# sudo systemctl enable bluetooth
